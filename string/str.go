f1b4d3ad4dbcf8b9bfb7b2a887d293a3ad4d207f
unknown <huanglongchao@rong360ad.com>
2019-09-23 18:49:02 +0800

    coding

--- string/str.go
+++ string/str.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main(){
 	m := map[string]int{
 		"abc": 123,
@@ -9,4 +11,12 @@ func main(){
 	x,ok := m[string(key)]
 
 	println(x,ok)
+
+	s := "雨痕"
+	for i:=0; i < len(s); i++ {	//byte
+		fmt.Printf("%d: [%c]\n",i , s[i])
+	}
+	for i,c := range s{			//rune
+		fmt.Printf("%d: [%c]\n", i, c)
+	}
 }
